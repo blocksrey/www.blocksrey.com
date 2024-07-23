c9aa3db25e02f256e7e9a1edcac351039df76935
Jeffrey Skinner <jeff@je.gy>
2024-07-23 21:24:32 +0900

    neos -> banners, remove lower-cased keys, fav.webp -> fav.gif

--- shared/favs/normalize.sh
+++ shared/favs/normalize.sh
@@ -1,11 +1,23 @@
+# for f in *.ico; do
+# 	n=`basename $f .ico`
+# 	ffmpeg -loglevel warning -y -i $f -lossless 1 -vf scale=-1:16:flags=neighbor ../icons/$n.webp
+# done
+
+# cd ../icons
+
+# for f in *.webp; do
+# 	n=`basename $f .webp`
+# 	ffmpeg -loglevel warning -y -i $f -lossless 1 -vf scale=-1:32:flags=neighbor ../icons/$n.webp
+# done
+
 for f in *.ico; do
 	n=`basename $f .ico`
-	ffmpeg -loglevel warning -y -i $f -lossless 1 -vf scale=-1:16:flags=neighbor ../icons/$n.webp
+	ffmpeg -loglevel warning -y -i $f -lossless 1 -vf scale=-1:16:flags=neighbor ../icons/$n.gif
 done
 
 cd ../icons
 
-for f in *.webp; do
-	n=`basename $f .webp`
-	ffmpeg -loglevel warning -y -i $f -lossless 1 -vf scale=-1:32:flags=neighbor ../icons/$n.webp
+for f in *.gif; do
+	n=`basename $f .gif`
+	ffmpeg -loglevel warning -y -i $f -lossless 1 -vf scale=-1:32:flags=neighbor ../icons/$n.gif
 done

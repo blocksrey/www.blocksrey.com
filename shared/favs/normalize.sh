7d784ce05a7cd361cb55de78661d4e377a5c746e
Jeffrey Skinner <jeff@je.gy>
2024-07-23 22:15:04 +0900

    Converter now outputs gifs nicely

--- shared/favs/normalize.sh
+++ shared/favs/normalize.sh
@@ -1,3 +1,4 @@
+# webp version
 # for f in *.ico; do
 # 	n=`basename $f .ico`
 # 	ffmpeg -loglevel warning -y -i $f -lossless 1 -vf scale=-1:16:flags=neighbor ../icons/$n.webp
@@ -10,14 +11,19 @@
 # 	ffmpeg -loglevel warning -y -i $f -lossless 1 -vf scale=-1:32:flags=neighbor ../icons/$n.webp
 # done
 
+
+
+# gif version
 for f in *.ico; do
 	n=`basename $f .ico`
-	ffmpeg -loglevel warning -y -i $f -lossless 1 -vf scale=-1:16:flags=neighbor ../icons/$n.gif
+	ffmpeg -loglevel warning -y -i $f -lossless 1 ../icons/$n.webp
 done
 
 cd ../icons
 
-for f in *.gif; do
-	n=`basename $f .gif`
-	ffmpeg -loglevel warning -y -i $f -lossless 1 -vf scale=-1:32:flags=neighbor ../icons/$n.gif
+for f in *.webp; do
+	n=`basename $f .webp`
+	# im not sure but i saw some 30x30 things (https://theoldnet.com/)
+	ffmpeg -loglevel warning -y -i "$f" -lavfi "palettegen=reserve_transparent=1:transparency_color=black[p];[0:v][p]paletteuse,format=rgba,scale=30:-1:flags=lanczos" "../icons/$n.gif"
+	rm "$n.webp"
 done
